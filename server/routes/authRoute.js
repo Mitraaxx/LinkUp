95591a4606cba66fa79b04372b7dcb98a80901eb
Priyanshu Mitra <priyanshumitra986@gmail.com>
2025-06-20 02:39:36 +0530

    done

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Signup, Login, LogOut } = require('../controller/auth');
-const { isLogin } = require('../middleware/isLogIn');
+
 
 const router = express.Router();
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAllUsers } = require('../controller/user');
-const { isLogin } = require('../middleware/isLogIn');
+
 
 
 const router = express.Router();
